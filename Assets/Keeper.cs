792c26711adadce6daae56b0f51925288e257123
ao_181 <m0119002aa@edu.teu.ac.jp>
2020-11-03 00:30:53 +0900

    飼育員とその挙動

--- Assets/Keeper.cs
+++ Assets/Keeper.cs
@@ -20,13 +20,19 @@ public class Keeper : MonoBehaviour
             case true:
                 transform.position += new Vector3(1.0f * Time.deltaTime, 0.0f, 0.0f);
                 if (transform.position.x >= 8.0)
+                {
                     Keepermove = false;
+                    transform.Rotate(new Vector3(0, 0, 180));
+                }                
                 break;
 
             case false:
                 transform.position -= new Vector3(1.0f * Time.deltaTime, 0.0f, 0.0f);
                 if (transform.position.x <= 4.0)
+                {
                     Keepermove = true;
+                    transform.Rotate(new Vector3(0, 0, -180));
+                }                        
                 break;
         }
     }

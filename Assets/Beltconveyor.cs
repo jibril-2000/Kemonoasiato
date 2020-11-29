86475f09bc199bbf2e3a49b92baf884ff30e880d
Takasu <m01191694a@edu.teu.ac.jp>
2020-11-29 18:41:44 +0900

    ベルトコンベア完成版、ポーズ画面、ギミックの説明UI追加

--- Assets/Beltconveyor.cs
+++ Assets/Beltconveyor.cs
@@ -4,52 +4,51 @@ using UnityEngine;
 
 public class Beltconveyor : MonoBehaviour
 {
-    public bool IsOn = false;
-    public float TargetDriveSpeed = 3.0f;
-    public float CurrentSpeed { get { return _currentSpeed; } }
-    public Vector3 DriveDirection = Vector3.forward;
-    [SerializeField] private float _forcePower = 50f;
-    private float _currentSpeed = 0;
-    private List<Rigidbody> _rigidbodies = new List<Rigidbody>();
+    RectTransform canvasRect;
+    public GameObject UI;
+
+    private void Awake()
+    {
+        canvasRect = GameObject.Find("Button").GetComponent<RectTransform>();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        //方向は正規化
-        DriveDirection = DriveDirection.normalized;
+
     }
 
-    void FixedUpdate()
-    {
-        _currentSpeed = IsOn ? TargetDriveSpeed : 0;
-
-        _rigidbodies.RemoveAll(r => r == null);
-
-        foreach(var r in _rigidbodies)
-        {
-            var objectSpeed = Vector3.Dot(r.velocity, DriveDirection);
-            if(objectSpeed < Mathf.Abs(TargetDriveSpeed))
-            {
-                r.AddForce(DriveDirection * _forcePower,
-                           ForceMode.Acceleration);
-            }
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        var rigidBody = collision.gameObject.GetComponent<Rigidbody>();
-        _rigidbodies.Add(rigidBody);
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        var rigidbody = collision.gameObject.GetComponent<Rigidbody>();
-        _rigidbodies.Remove(rigidbody);
-    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void OnMouseDrag()
+    {
+        Vector3 objectPointInScreen
+            = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        Vector3 mousePointInScreen
+            = new Vector3(Input.mousePosition.x,
+                          Input.mousePosition.y,
+                          objectPointInScreen.z);
+
+        Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+        mousePointInWorld.z = this.transform.position.z;
+        this.transform.position = mousePointInWorld;
+    }
+
+    //マウスが乗っている間、呼び出され続ける
+    void OnMouseOver()
+    {
+        UI.SetActive(true);
+    }
+
+    //マウスが離れたとき
+    void OnMouseExit()
+    {
+        UI.SetActive(false);
+    }
+
 }
---
+++ Assets/GearSupin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearSupin : MonoBehaviour
+{
+    float rotation_speed = 0; // 回転速度
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.rotation_speed = -5.0f;
+        transform.Rotate(0, 0, this.rotation_speed);
+    }
+}

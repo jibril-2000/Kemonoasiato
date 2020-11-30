5acd25d1319ce4d55e114a624b8a2d9ac2d2fda0
jibril-2000 <ookusi2000@gmail.com>
2020-11-30 23:30:33 +0900

    アルファに至っての調整や画像の追加、sceneの追加

---
+++ Assets/TitleBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleBack : MonoBehaviour
+{
+    
+
+    private void Start()
+    {
+        
+    }
+
+    public void TitleLord()             //　ReplayGame()メソッドです
+    {
+        SceneManager.LoadScene("Title"); 
+    }
+}

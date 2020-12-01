2ba1a6f916fdbe179e3e19fc9795abeda8fd77c3
Kinoshita <m01191002e@edu.teu.ac.jp>
2020-12-02 04:05:03 +0900

    トランポリン、システム、カラス、地面に落ちた音、ベルトコンベアのサウンド実装 一部グラフィックの修正

---
+++ Assets/SoundScript/ButtonPushSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonPushSound : MonoBehaviour
+{
+    public CriAtomSource criAtomSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    
+    public void PlaySound()
+    {
+        //サウンド再生
+        criAtomSource.Play();
+
+    }
+}
---
+++ Assets/SoundScript/FallSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallSound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "Namakemono")
+        {
+            //サウンド再生
+            CriAtomSource audio = (CriAtomSource)GetComponent("CriAtomSource");
+            audio.Play();
+        }
+    }
+}

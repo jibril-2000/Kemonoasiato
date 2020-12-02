22a5fc716f1f2a4832eb70131445c013331932c1
jibril-2000 <ookusi2000@gmail.com>
2020-12-02 11:52:27 +0900

    アルファ版修正

---
+++ Assets/Quit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+
+    }
+
+    public void quit()             //　ReplayGame()メソッドです
+    {
+
+      UnityEngine.Application.Quit();
+
+    }
+}
---
+++ Assets/RotateObj.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObj : MonoBehaviour
+{
+    Vector3 firstPos;
+    Vector3 lastPos;
+
+    private Renderer _renderer; 
+
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>(); //
+        _renderer.enabled = false; //trueで表示
+    }
+
+    // Update is called once per frame
+    void OnMouseEnter()
+    {
+        _renderer.enabled = true;
+    }
+
+    void OnMouseDrag()
+    {
+
+        _renderer.enabled = true;
+        float sensitivity = 1f; // いわゆるマウス感度
+        float mouse_move_x = Input.GetAxis("Mouse X")*2 ;
+        float mouse_move_y = Input.GetAxis("Mouse Y")*2 ;
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstPos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            lastPos = Input.mousePosition;
+        }
+
+            var dic = lastPos - firstPos;
+
+            if (dic.y > 1.0f)
+            {
+                gameObject.transform.parent.Rotate(0,0, mouse_move_x+ mouse_move_y);
+            }
+           
+        
+    }
+    void OnMouseExit()
+    {
+        _renderer.enabled = false;
+    }
+
+}
+

9b8f0be2ba3c2f3c098d072080022cd706c81423
jibril-2000 <ookusi2000@gmail.com>
2020-12-08 01:35:17 +0900

    移動制限と自動リスタートです

---
+++ Assets/Script/Ground.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    public GameObject Player;
+    Namakemono script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = Player.gameObject.GetComponent<Namakemono>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Namakemono"))
+        {
+            StartCoroutine("Resporn");//コルーチンを使いたいところにこれを入れる
+            
+        }
+
+    }
+    public IEnumerator Resporn()
+    {
+       
+            yield return new WaitForSeconds(1);//何秒待つのか
+            Player.gameObject.transform.position = script.Resporn;
+            
+
+        
+    }
+}

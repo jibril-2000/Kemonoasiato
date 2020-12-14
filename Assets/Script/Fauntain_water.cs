8df486647e92067a3c68d864f4553a7d1be57ed1
Takasu <m01191694a@edu.teu.ac.jp>
2020-12-15 02:33:22 +0900

    噴水、ネットの追加

---
+++ Assets/Script/Fauntain_water.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fauntain_water : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //噴水の水の当たり判定を消す
+        GetComponent<Collider2D>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //右クリックで当たり判定出現
+        if (Input.GetMouseButton(1))
+        {
+            GetComponent<Collider2D>().enabled = true;
+        }
+        //左クリックで当たり判定消す
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+}
---
+++ Assets/Script/Fountain.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fountain : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //はじめは何も作動してないようにする
+        if (animator.GetCurrentAnimatorStateInfo(0).IsTag("Water"))
+        {          
+            animator.SetBool("Water", false);
+            return;
+        }
+        
+    }
+
+    //マウスが噴水に合わせてあるとき
+    private void OnMouseOver()
+    {
+        //右クリックで
+        if (Input.GetMouseButton(1))
+        {
+            animator.SetBool("Water", true);
+        }
+
+        //左クリックで
+        if(Input.GetMouseButton(0))
+        {
+            animator.SetBool("Reverse", true);
+            animator.SetBool("Water", false);
+        }
+        else
+        {
+            animator.SetBool("Reverse", false);
+        }
+    }
+}

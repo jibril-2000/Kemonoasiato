e0e915e813fd7429a2e1df5a363df717c97b02fd
Takasu <m01191694a@edu.teu.ac.jp>
2020-12-15 16:40:15 +0900

    通風口とその風スクリプト追加

---
+++ Assets/Script/Vents_Down.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vents_Down : MonoBehaviour
+{
+    public GameObject vents_up;
+    public GameObject vents_down;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            vents_up.SetActive(true);
+            vents_down.SetActive(false);
+        }
+    }
+}

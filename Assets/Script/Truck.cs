3ab583b98e4c1b297224dee39f81f2155b3d8425
jibril-2000 <ookusi2000@gmail.com>
2020-12-16 22:28:34 +0900

    修正

--- Assets/Script/Truck.cs
+++ Assets/Script/Truck.cs
@@ -8,32 +8,41 @@ public class Truck : MonoBehaviour
 {
     public GameObject stageclear;
     public GameObject truck_body;
+    GameObject MainBGM;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnTriggerStay2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Namakemono")
         {
-            truck_body.transform.Translate(3.0f * Time.deltaTime, 0.0f,0.0f);
+            //サウンド再生
+            CriAtomSource audio = (CriAtomSource)GetComponent("CriAtomSource");
+            audio.Play();
+            //BGMを止める
+            MainBGM = GameObject.Find("Main Camera");
+            MainBGM.GetComponent<CriAtomSource>().Stop();
+
+            truck_body.transform.Translate(3.0f * Time.deltaTime, 0.0f, 0.0f);
         }
         StartCoroutine("NextStage");//コルーチンを使いたいところにこれを入れる
     }
     public IEnumerator NextStage()
     {
-        
+
         yield return new WaitForSeconds(3);//何秒待つのか
 
         SceneManager.LoadScene("Game2");
 
     }
 }
+

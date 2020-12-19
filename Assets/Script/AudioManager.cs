9370084556c414ab006fc21fe5a814951192ba5d
Takasu <m01191694a@edu.teu.ac.jp>
2020-12-19 22:05:56 +0900

    サウンド調節機能追加

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioClip[] bgmList;//BGMを読み込む
+    [SerializeField] AudioSource audioSourceBGM;//BGMの音の大きさを調節するために読み込む
+
+    //SecneをまたいでもObjectが破壊されないようにする
+    static AudioManager Instance = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //BGMのボリューム調節する関数を作成
+    public float BGMVolume
+    {
+        //audioSourceBGMのvolumeをいじることでBGMを調整
+        get { return audioSourceBGM.volume; }
+        set { audioSourceBGM.volume = value; }
+    }
+
+    public static AudioManager GetInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = FindObjectOfType<AudioManager>();
+        }
+        return Instance;
+    }
+
+    private void Awake()
+    {
+        if (this != GetInstance())
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    //BGMを再生する関数を作成
+    public void PlayBGM(int index)
+    {
+        audioSourceBGM.clip = bgmList[index];
+        audioSourceBGM.Play();
+    }
+}
---
+++ Assets/Script/SoundFeeder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundFeeder : MonoBehaviour
+{
+    //BGM
+    [SerializeField] Text bgmtext;
+    [SerializeField] Slider bgmslider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.GetInstance().PlayBGM(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnChangedBGMSlider()
+    {
+        //Sliderの値に応じてBGMを変更
+        AudioManager.GetInstance().BGMVolume = bgmslider.value;
+        //volumeTextの値をSliderのvalueに変更
+        bgmtext.text = string.Format("{0:0}", bgmslider.value * 100);
+    }
+}

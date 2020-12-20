cb508482dc4269a194b3bf231fd2ff70c914d8b4
ao_181 <m0119002aa@edu.teu.ac.jp>
2020-12-21 00:19:57 +0900

    ナマケモノに入れるパチンコ用スクリプト修正、パチンコに入れるマテリアル追加

--- Assets/Script/Namakemonopachinko.cs
+++ Assets/Script/Namakemonopachinko.cs
@@ -5,16 +5,14 @@ using UnityEngine;
 public class Namakemonopachinko : MonoBehaviour
 {
     public GameObject pachinkostart;
-    public GameObject pachinkoFind;
+
     Rigidbody2D rigid2d;
     Vector2 startPos;
-    private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        this.rigid2d = GetComponent<Rigidbody2D>();
-        this.speed = 500;
+        
     }
 
     // Update is called once per frame
@@ -28,31 +26,24 @@ public class Namakemonopachinko : MonoBehaviour
         if (other.gameObject.tag == "Pachinkobody")
         {
             transform.position = pachinkostart.transform.position;
-            pachinkoFind.SetActive(false);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Pachinko")
+        {
+            transform.rotation = Quaternion.identity;//向きを元に戻す
         }
     }
     private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.tag == "Pachinko")
         {
-            if (Input.GetMouseButtonDown(1))
+
+            if (Input.GetMouseButton(1))
             {
-                Debug.Log("a");
-                this.startPos = Input.mousePosition;
-            }
-            else if (Input.GetMouseButtonUp(1))
-            {
-                Debug.Log("a");
-                Vector2 endPos = Input.mousePosition;
-                Vector2 startDirection = -1 * (endPos - startPos).normalized;
-                this.rigid2d.AddForce(startDirection * speed);
+                GetComponent<Rigidbody2D>().velocity = (transform.up*9);//ナマケモノから見て上方向に力を加える
             }
         }
-    }
-
-
-    void FixedUpdate()
-    {
-        this.rigid2d.velocity *= 0.995f;
     }
 }

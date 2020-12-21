dff08b6594b4ada352074f57fc78fa3d7f0cddce
ao_181 <m0119002aa@edu.teu.ac.jp>
2020-12-22 00:18:32 +0900

    ゴールテキストプログラムと仮のテキスト画像の追加

---
+++ Assets/Script/Truckgoal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truckgoal : MonoBehaviour
+{
+    Animation anim;
+    public GameObject karigoal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = karigoal.gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Namakemono")
+        {
+            anim.Play();
+        }
+    }
+}

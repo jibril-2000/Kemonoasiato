a2deb42b6078b291ba8e05a3cda559ff0797b680
jibril-2000 <ookusi2000@gmail.com>
2020-12-23 22:28:46 +0900

    トランポリン修正とスクリプト調整

--- Assets/SoundScript/FallSound.cs
+++ Assets/SoundScript/FallSound.cs
@@ -16,7 +16,7 @@ public class FallSound : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Namakemono")
         {

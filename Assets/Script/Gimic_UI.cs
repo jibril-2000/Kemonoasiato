607353b8cdc4a34f305bacfc6433b41fc8305393
jibril-2000 <ookusi2000@gmail.com>
2021-01-03 00:23:31 +0900

    ギミック説明文差し替えと変更

--- Assets/Script/Gimic_UI.cs
+++ Assets/Script/Gimic_UI.cs
@@ -9,10 +9,13 @@ public class Gimic_UI : MonoBehaviour
     public float interval = 1.5f;
     float timer;
     public float speed = 0.01f;
+    bool OnMouse = false;
+    float alpha = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
@@ -24,17 +27,31 @@ public class Gimic_UI : MonoBehaviour
 
             if (timer > interval)
             {
-                UI.SetActive(false);
+                //UI.SetActive(false);
                 timer = 0;
             }
         }
     }
 
+    void OnMouseEnter()
+    {
+        if (OnMouse == false)
+        {
+
+            StartCoroutine("Pop");
+            //StopCoroutine("Negate");
+            //Invoke("Vanish", 1);
+        }
+        OnMouse = true;
+
+
+
+
+    }
     //マウスが乗っている間、呼び出され続ける
     void OnMouseOver()
     {
-        Invoke("Vanish", 1);
-        
+        StopCoroutine("Negate");
     }
 
     void Vanish()
@@ -46,5 +63,43 @@ public class Gimic_UI : MonoBehaviour
     void OnMouseExit()
     {
         
+        StartCoroutine("Negate");
+        OnMouse = false;
+
+    }
+    public IEnumerator Pop()
+    {
+        StopCoroutine("Negate");
+        for (float i = 0; i < 1f; i = i + 0.1f)
+        {
+            UI.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, alpha+=0.1f);
+
+            Debug.Log(UI.GetComponent<Image>().color);
+            alpha = alpha + i;
+            if (alpha > 1.1f)
+            {
+                alpha = 1f;
+            }
+            yield return new WaitForSeconds(0.1f);//何秒待つのか
+            
+        }
+        
+    }
+    public IEnumerator Negate()
+    {
+        yield return new WaitForSeconds(0.5f);//何秒待つのか
+        for (float i = 0; i <100000; i = i + 0.1f)
+        {
+            UI.GetComponent<Image>().color = new Color(1.0f, 1.0f, 1.0f, alpha-=i);
+            Debug.Log(UI.GetComponent<Image>().color);
+            alpha = alpha - i;
+            if (alpha<-0.1f)
+            {
+                alpha = 0;
+                //StopCoroutine("Negate");
+            }
+            yield return new WaitForSeconds(0.1f);//何秒待つのか
+        }
+
     }
 }
